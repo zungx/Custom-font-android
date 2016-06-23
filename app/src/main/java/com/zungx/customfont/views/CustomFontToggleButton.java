b9cc4f0aabd85d563d35002a4ef7397acaac7671
Van Dung <vandung@NAL03.local>
2016-06-23 10:37:45 +0700

    add new view

---
+++ app/src/main/java/com/zungx/customfont/views/CustomFontToggleButton.java
@@ -0,0 +1,58 @@
+package com.zungx.customfont.views;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.ToggleButton;
+
+import com.zungx.customfont.utils.CustomFontUtils;
+
+/**
+ * Created by vandung on 6/23/16.
+ */
+public class CustomFontToggleButton extends ToggleButton {
+    /**
+     * Default View Constructor.
+     *
+     * @param context application context.
+     */
+    public CustomFontToggleButton(final Context context) {
+        super(context);
+    }
+
+    /**
+     * Default View Constructor. Sets custom font with no style.
+     *
+     * @param context application context.
+     * @param attrs   View attributes.
+     */
+    public CustomFontToggleButton(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    /**
+     * Default View Constructor. Sets custom font with style.
+     *
+     * @param context  application context.
+     * @param attrs    View attributes.
+     * @param defStyle View Style.
+     */
+    public CustomFontToggleButton(final Context context, final AttributeSet attrs,
+                                  final int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs);
+    }
+
+    /**
+     * Initialize custom font attribute.
+     *
+     * @param attrs attributes.
+     */
+    private void init(final AttributeSet attrs) {
+        Typeface typeface;
+        if (!isInEditMode() && (typeface = CustomFontUtils.getTypeFace(this, attrs)) != null) {
+            super.setTypeface(typeface, typeface.getStyle());
+        }
+    }
+}
